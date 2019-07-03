fddd47eda72e0d7b5b2af13b07ad1523527f0d96
Matteo Carotta <matteo.carotta@gmail.com>
2019-07-03 20:18:38 +0200

    Created p5.js Project template

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    name: "P5.jsProjectTemplate",
+
+    directory: false,
+
+    params: ["Name"],
+
+    rules: function(config) {
+
+      return({
+        items: [
+          { destinationFile: "index.html", sourceTemplateFile: "index.template"},
+          { destinationFile: "scripts/sketch.js", sourceContentFile: "scripts/sketch.js" },
+          { destinationFile: "styles/style.css", sourceContentFile: "styles/style.css" },
+          { destinationFile: "libraries/p5.js", sourceContentFile: "node_modules/p5/lib/p5.js" },
+          { destinationFile: "libraries/p5.min.js", sourceContentFile: "node_modules/p5/lib/p5.min.js" },
+          { destinationFile: "libraries/p5.dom.js", sourceContentFile: "node_modules/p5/lib/addons/p5.dom.js" },
+          { destinationFile: "libraries/p5.dom.min.js", sourceContentFile: "node_modules/p5/lib/addons/p5.dom.min.js" },
+          { destinationFile: "libraries/p5.sound.js", sourceContentFile: "node_modules/p5/lib/addons/p5.sound.js" },
+          { destinationFile: "libraries/p5.sound.min.js", sourceContentFile: "node_modules/p5/lib/addons/p5.sound.min.js" }
+        ]
+      });
+
+    }
+
+}
---
+++ scripts/sketch.js
@@ -0,0 +1,10 @@
+// Whole-script strict mode syntax
+'use strict';
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
